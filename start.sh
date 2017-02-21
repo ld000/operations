17bca4037b30e58287d8480515a11611194ffc96
ld0000 <lidong9144@163.com>
2017-02-21 10:19:03 +0800

    push

--- start.sh
+++ start.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
+# mysql setting
+export DATA_SOURCE_NAME='login:password@(hostname:port)/database'
+
 usage() {
-  echo usage
+cat <<-EOF
+   usage:
+
+    First change DATA_SOURCE_NAME in start.sh.
+    Then change prometheus/prometheus.yml line 8 node-exporter, mysqld-exporter to your IP address.
+
+    start     ./start.sh start
+    stop      ./start.sh stop
+
+EOF
 }
 
 [[ -z "$1" ]] && { usage; }
 
 set -u
 
-start_func() {
-  export DATA_SOURCE_NAME='login:password@(hostname:port)/'
+which docker-compose &> /dev/null
 
+if [ $? -gt 0 ]; then
+  echo "Can not find docker-compose! Please install docker and docker-compose first."
+  exit 1
+fi
+
+if [ "$(whoami)" != "root" ]; then
+  if [ -f `which sudo` ]; then
+    sudo $0 "$@"
+    exit $?
+  else
+    usage
+  fi
+fi
+
+start_func() {
 #  ./exporter/mysqld_exporter -collect.binlog_size=true -collect.info_schema.processlist=true &
 
   ./exporter/node_exporter -collectors.enabled="diskstats,filefd,filesystem,loadavg,meminfo,netdev,stat,time,uname,vmstat" &
@@ -23,18 +49,20 @@ stop_func() {
   mysqld_pid=$(pidof mysqld_exporter)
 
   if [ -n "$node_pid" ]; then
+    echo "stop node_exporter..."
     kill -9 $node_pid
   fi
 
   if [ -n "$mysqld_pid" ]; then
+    echo "stop mysqld_exporter..."
     kill -9 $mysqld_pid
   fi
+
+  echo "stop docker-compose..."
   docker-compose stop
 }
 
-echo $1
-
-if [ "$1" == "art" ]; then
+if [ "$1" == "start" ]; then
   start_func
 elif [ "$1" == "stop" ]; then
   stop_func
